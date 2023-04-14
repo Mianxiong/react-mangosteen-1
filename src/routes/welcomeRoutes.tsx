10e262f9578b0d9e0a22d0b90dd1d6990388a9aa
Mianxiong <757134184@qq.com>
2023-04-14 10:47:03 +0800

    添加一个默认的404页面

--- src/routes/welcomeRoutes.tsx
+++ src/routes/welcomeRoutes.tsx
@@ -1,12 +1,14 @@
+import { RouteObject } from "react-router-dom";
 import { WelcomeLayout } from "../layouts/WelcomeLayout";
 import { Welcome1 } from "../pages/Welcome1";
 import { Welcome2 } from "../pages/Welcome2";
 import { Welcome3 } from "../pages/Welcome3";
 import { Welcome4 } from "../pages/Welcome4";
 
-export const welcomeRoutes = {
+export const welcomeRoutes: RouteObject = {
     path: 'welcome',
     element: <WelcomeLayout />,
+    errorElement: <div>加载失败</div>,
     children: [
         { path: '1', element: <Welcome1 /> },
         { path: '2', element: <Welcome2 /> },
