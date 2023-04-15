287f91c1c4423d40f84fdc0cf4acecf0522d2f29
Mianxiong <757134184@qq.com>
2023-04-15 14:11:30 +0800

    如何发送关联请求

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,21 @@
 import p from '../assets/images/pig.svg'
 import s from './Home.module.scss'
 import add from '../assets/icons/add.svg'
+import { useEffect } from 'react';
+import axios from 'axios';
+import useSWR from "swr";
 
 export const Home: React.FC = () => {
+    const { data: meData, error: meError } = useSWR('/api/v1/me', (path) => {
+        return axios.get(`http://121.196.236.94:8080${path}`)
+    })
+    const {data: itemsData, error: itemsError} = useSWR(meData ? '/api/v1/items' : null, (path) => {
+        return axios.get(`http://121.196.236.94:8080${path}`)
+    })
+    // useEffect(() => {
+    //     axios.get('http://121.196.236.94:8080/api/v1/me')
+    //     .then(() => {}, ()=>{})
+    // })
     return <div>
         <div className={s.imgBox}>
             <img src={p} className={s.pig} />
