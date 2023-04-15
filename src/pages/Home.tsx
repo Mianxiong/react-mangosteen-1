c6a441702c1b0e5170910e3bbc93d5ee32e842b9
Mianxiong <757134184@qq.com>
2023-04-15 15:29:14 +0800

    开发环境和生产环境访问不同的API接口

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,13 +4,14 @@ import add from '../assets/icons/add.svg'
 import { useEffect } from 'react';
 import axios from 'axios';
 import useSWR from "swr";
+import { ajax } from '../lib/ajax';
 
 export const Home: React.FC = () => {
     const { data: meData, error: meError } = useSWR('/api/v1/me', (path) => {
-        return axios.get(`http://121.196.236.94:8080${path}`)
+        return ajax.get(path)
     })
     const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, (path) => {
-        return axios.get(`http://121.196.236.94:8080${path}`)
+        return ajax.get(path)
     })
     console.log(meData, meError, itemsData, itemsError)
     // mock
