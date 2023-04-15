bdc60cad198455ede37af44daa4b31e131d480a4
Mianxiong <757134184@qq.com>
2023-04-15 22:05:00 +0800

    封装 Loading.tsx 组件

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,30 @@
-export const Loading: React.FC = () => {
+import styled from "styled-components"
+import { Icon } from "./Icon"
+import s from './Loading.module.scss'
+import c from 'classnames'
+
+const Div = styled.div`
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  svg {
+    animation: spin 1.25s linear infinite;
+  }
+`
+interface Props {
+  className?: string
+  message?: string
+}
+export const Loading: React.FC<Props> = ({ className, message }) => {
   return (
-    <div>加载中 </div>
+    <Div className={c(s.wrapper, className)}>
+      <Icon name="loading" className={s.loading} />
+      <p>{message || '加载中......'}</p>
+    </Div>
   )
 }
