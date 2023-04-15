0279ecb9af3568840a4f575e44749855f2f9709a
Mianxiong <757134184@qq.com>
2023-04-15 11:17:27 +0800

    完成首页样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+import p from '../assets/images/pig.svg'
+import s from './Home.module.scss'
+import add from '../assets/icons/add.svg'
+
 export const Home: React.FC = () => {
-    return <div>Home</div>
+    return <div>
+        <div className={s.imgBox}>
+            <img src={p} className={s.pig} />
+        </div>
+        <div className={s.btnBox}>
+            <button className={s.btn}>开始记账</button>
+        </div>
+
+        <button className={s.btnBottom}>
+            <img src={add} />
+        </button>
+    </div>
 }
