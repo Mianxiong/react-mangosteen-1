a25bc62e75841bde81437d1865ad931e4acccf65
Mianxiong <757134184@qq.com>
2023-04-16 16:45:39 +0800

    完成时间范围选择组件 TimeRangePicker

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -1,5 +1,31 @@
-export const TimeRangePicker: React.FC = () => {
-  return (
-    <div>TimeRangePicker </div>
-  )
+import s from './TimeRangePicker.module.scss'
+export type TimeRange = 'thisMonth' | 'lastMonth' | 'thisYear' | 'custom'
+interface Props {
+    selected: TimeRange
+    onSelected: (selected: TimeRange) => void
+}
+export const TimeRangePicker: React.FC<Props> = ({ selected, onSelected }) => {
+    const timeRanges: { key: TimeRange; text: string }[] = [
+        { key: 'thisMonth', text: '本月' },
+        { key: 'lastMonth', text: '上月' },
+        { key: 'thisYear', text: '今年' },
+        { key: 'custom', text: '自定义时间' }
+    ]
+    return (
+        <div>
+            {/* <ol className={s.wrapper}>
+                <li>本月</li>
+                <li>上个月</li>
+                <li>今年</li>
+                <li>自定义时间</li>
+            </ol> */}
+            <ol className={s.wrapper}>
+                {timeRanges.map(tr => <li key={tr.key} className={tr.key === selected ? s.selected : ''} onClick={() =>
+                    onSelected(tr.key)
+                }>
+                    {tr.text}
+                </li>)}
+            </ol>
+        </div>
+    )
 }
--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,20 +1,23 @@
 import { AddItemFloatButton } from "../components/AddItemFloatButton"
-import { TimeRangePicker } from "../components/TimeRangePicker"
+import { TimeRangePicker, TimeRange } from '../components/TimeRangePicker';
 import { Topnav } from "../components/Topnav"
 import { ItemsList } from "./ItemsPage/ItemsList"
 import { ItemsSummary } from "./ItemsPage/ItemsSummary"
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const Div = styled.div`
   background: linear-gradient(0deg,rgba(143,76,215,1) 0%,rgba(92,51,190,1) 100%);
 `
 
 export const ItemsPage: React.FC = () => {
+  const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth') //useState类型是TimeRange
   return (
     <div>
       <Div>
         <Topnav />
-        <TimeRangePicker />
+        {/* <TimeRangePicker selected={timeRange} onSelected={key => setTimeRange(key)}/> */}
+        <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
       </Div>
 
       <ItemsSummary />
