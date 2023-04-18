e0d8b9146969aae6b43f08c994aa5b74f7322609
Mianxiong <757134184@qq.com>
2023-04-18 15:11:24 +0800

    表单提交事件可以获取到表单数据

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -3,7 +3,14 @@ import { Gradient } from '../components/Gradient';
 import { Icon } from "../components/Icon";
 import s from './SignInPage.module.scss';
 import c from 'classnames'
+import { FormEventHandler } from "react";
+import { useSignInStore } from "../stores/useSignInStore";
 export const SignInPage: React.FC = () => {
+    const { data, setData } = useSignInStore()
+    const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+        e.preventDefault()
+        console.log(data)
+    }
     return (
         <div>
             <Gradient>
@@ -15,15 +22,16 @@ export const SignInPage: React.FC = () => {
                 <Icon name="logo" className={s.logo}/>
                 <h1>山竹记账</h1>
             </div>
-            <form className={s.form}>
+            <form className={s.form} onSubmit={onSubmit}>
+                <div style={{ border: '1px solid red'}}>{JSON.stringify(data)}</div>
                 <div>
                     <span className={s.label}>邮箱地址</span>
-                    <input type="text" placeholder='请输入邮箱，然后点击发送验证码' className={s.input}/>
+                    <input type="text" placeholder='请输入邮箱，然后点击发送验证码' className={s.input} value={data.email} onChange={e => setData({email: e.target.value})}/>
                 </div>
                 <div>
                     <span className={s.label}>验证码</span>
                     <div className={s.verification}>
-                        <input type="text" placeholder='六位数字' className={c(s.input, s.inputVerification)}/>
+                        <input type="text" placeholder='六位数字' className={c(s.input, s.inputVerification)} value={data.code} onChange={e => setData({code: e.target.value})}/>
                         <button className={c(s.btn,s.btn_ml)}>发送验证码</button>
                     </div>
                 </div>
