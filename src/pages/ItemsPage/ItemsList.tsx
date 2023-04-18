abd62d2eb5f5127b3fa98ebaa7ee70b76720d087
Mianxiong <757134184@qq.com>
2023-04-18 09:38:43 +0800

    请求所有页数的数据

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -3,34 +3,50 @@ import useSWRInfinite from 'swr/infinite'
 import { ajax } from '../../lib/ajax'
 interface Props {
 }
-const getKey = (pageIndex: number) => {
+const getKey = (pageIndex: number, prev: Resources<Item>) => {
+    if (prev) {
+        const sendCount = (prev.pager.page - 1) * prev.pager.per_page + prev.resources.length
+        const count = prev.pager.count
+        if (sendCount >= count) {
+            return null
+        }
+    }
     return `/api/v1/items?page=${pageIndex + 1}`
 }
 export const ItemsList: React.FC<Props> = () => {
-    const { data, error } = useSWRInfinite(
+    const { data, error, size, setSize } = useSWRInfinite(
         getKey, async (path) => (await ajax.get<Resources<Item>>(path)).data
     )
-    const items:Item[] = []
-    return <div>
-        <ol>
-            {items.map(item =>
-                <li key={item.id} className={s.li} gap-x-12px grid-cols="[auto_1fr_auto]">
-                    <div className={s.div1}>
-                        😊
-                    </div>
-                    <div className={s.div2}>
-                        旅行
-                    </div>
-                    <div className={s.div3}>
-                        2011年1月1日
-                    </div>
-                    <div className={s.div4}>
-                        ￥999
-                    </div>
-                </li>)}
-        </ol>
-        <div className={s.btnBox}>
-            <button className={s.btn}>加载更多</button>
+    const onLoadMore = () => {
+        setSize(size + 1)
+    }
+    if (!data) {
+        return <span>'还没搞定'</span>
+    } else {
+        return <div>
+            <ol>
+                {data.map(({ resources }) => {
+                    return resources.map(item =>
+                        <li key={item.id} className={s.li} gap-x-12px grid-cols="[auto_1fr_auto]">
+                            <div className={s.div1}>
+                                😊
+                            </div>
+                            <div className={s.div2}>
+                                旅行
+                            </div>
+                            <div className={s.div3}>
+                                2011年1月1日
+                            </div>
+                            <div className={s.div4}>
+                                ￥{item.amount / 100}
+                            </div>
+                        </li>
+                    )
+                })}
+            </ol>
+            <div className={s.btnBox}>
+                <button className={s.btn} onClick={onLoadMore}>加载更多</button>
+            </div>
         </div>
-    </div>
+    }
 }
