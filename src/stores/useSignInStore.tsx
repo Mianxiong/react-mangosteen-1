628d0b30425b79e53dc3a59b824e9e52ae66602f
Mianxiong <757134184@qq.com>
2023-04-19 15:43:13 +0800

    setError 时，之前的表单报错不会保留

--- src/stores/useSignInStore.tsx
+++ src/stores/useSignInStore.tsx
@@ -37,7 +37,6 @@ export const useSignInStore = create<SignIn>((set, get) => (
             set(state => ({
                 ...state,
                 error: {
-                    ...state.error,
                     ...error
                 }
             }
