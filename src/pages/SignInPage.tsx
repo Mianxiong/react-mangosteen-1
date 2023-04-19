627c982922290663bdf1f2e24415a12f7c9a73c2
Mianxiong <757134184@qq.com>
2023-04-19 16:09:42 +0800

    监听blur事件

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -5,19 +5,27 @@ import s from './SignInPage.module.scss';
 import c from 'classnames'
 import { FormEventHandler } from "react";
 import { useSignInStore } from "../stores/useSignInStore";
-import { validate } from '../lib/validate';
+import { validate, hasError } from '../lib/validate';
+import { ajax } from '../lib/ajax';
+const rules = [
+    { key: 'email', type: 'required', message: '请输入邮箱地址' },
+    { key: 'email', type: 'pattern', regex: /^.+@.+$/, message: '邮箱地址格式不正确' },
+    { key: 'code', type: 'required', message: '请输入验证码' },
+    { key: 'code', type: 'length', min: 4, max: 6, message: '验证码必须是6个字符' }
+]
 export const SignInPage: React.FC = () => {
     const { data, setData, error, setError } = useSignInStore()
+    const x = (key?: keyof typeof data) => {
+        const d = key ? { [key]: data[key] } : data
+        const r = key ? rules.filter(rule => rule.key === key) : rules
+        const error = validate(d, r as any)
+        console.log('error', error)
+        setError(error)   
+    }
     const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
         e.preventDefault()
-        const error = validate(data, [
-            { key: 'email', type: 'required', message: '请输入邮箱地址'},
-            { key: 'email', type: 'pattern', regex: /^.+@.+$/,message: '邮箱地址格式不正确'},
-            { key: 'code', type: 'required', message: '请输入验证码'},
-            { key: 'code', type: 'length', min: 4, max: 6, message: '验证码必须是6个字符'}
-        ])
-        console.log('error', error)
-        setError(error)
+        // x(data)
+        x()
     }
     return (
         <div>
@@ -31,15 +39,17 @@ export const SignInPage: React.FC = () => {
                 <h1>山竹记账</h1>
             </div>
             <form className={s.form} onSubmit={onSubmit}>
-                <div style={{ border: '1px solid red' }}>{JSON.stringify(data)}</div>
+                {/* <div style={{ border: '1px solid red' }}>{JSON.stringify(data)}</div> */}
                 <div>
                     <span className={s.label}>邮箱地址{error.email?.[0] && <span style={{ color: 'red' }}>{error.email[0]}</span>}</span>
                     <input type="text" placeholder='请输入邮箱，然后点击发送验证码' className={s.input} value={data.email} onChange={e => setData({ email: e.target.value })} />
+                    {/* onBlur={() => x('email')} */}
                 </div>
                 <div>
                     <span className={s.label}>验证码{error.code?.[0] && <span style={{ color: 'red' }}>{error.code[0]}</span>}</span>
                     <div className={s.verification}>
                         <input type="text" placeholder='六位数字' className={c(s.input, s.inputVerification)} value={data.code} onChange={e => setData({ code: e.target.value })} />
+                        {/* onBlur={() => x('code')} */}
                         <button className={c(s.btn, s.btn_ml)}>发送验证码</button>
                     </div>
                 </div>
