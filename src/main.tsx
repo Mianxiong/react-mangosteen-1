d8039030792144bb8650685cf6a0ff98073599ab
Mianxiong <757134184@qq.com>
2023-04-19 18:15:59 +0800

    解决手机浏览器上地址栏高度问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
+import vhCheck from 'vh-check'
 import { router } from './routes/router';
 import './global.scss'
 import 'virtual:uno.css'
 import 'virtual:svgsprites'
+
+const test = vhCheck()
 
 const div = document.getElementById("root") as HTMLElement;
 
