082eeb9e798fb13041984a28a619a535f250dc31
Mianxiong <757134184@qq.com>
2023-04-23 11:41:54 +0800

    完善 Tags 组件的样式细节

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,16 +1,23 @@
 import s from "./Tags.module.scss"
+import { Icon } from '../../components/Icon';
 type Props = {
     kind: Item['kind']
 }
 export const Tags: React.FC<Props> = (props) => {
-    const {kind} = props
+    const { kind } = props
     console.log(kind)
-    const tags = Array.from({ length: 99 })
+    const tags = Array.from({ length: 21 })
     return (
         <div>
             <ol className={s.Tags_ol}>
-                {tags.map(tag => 
-                    <li className={s.Tags_li}>😆</li>
+                <li>
+                    <span className={s.icon}><Icon name="add" /></span>
+                </li>
+                {tags.map(tag =>
+                    <li className={s.Tags_li}>
+                        <span className={s.top}>😆</span>
+                        <span className={s.bottom}>打车</span>
+                    </li>
                 )}
             </ol>
         </div>
