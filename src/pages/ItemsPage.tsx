b28434df52940676cfdea3ed50d7b0424cc04fe9
frankxiong <757134184@qq.com>
2023-09-05 19:44:21 +0800

    修改展示的字段

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -34,7 +34,7 @@ export const ItemsPage: React.FC = () => {
     return (
         <div>
             <Gradient>
-                <TopNav title="账目列表" icon={
+                <TopNav title="记账列表" icon={
                     <Icon name="menu" className="w-24px h-24px"
                           onClick={() => { setVisible(!visible) }}/>
                 }/>
